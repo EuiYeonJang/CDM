85374ee3cfd53f1496aa7c0ad034e371fed6c2af
Eui Yeon Jang <euiyeonjang@gmail.com>
2020-05-23 15:22:43 +0200

    preprocessing funcs

--- Alignment/alignment_utils.py
+++ Alignment/alignment_utils.py
@@ -4,55 +4,82 @@ import pickle as pkl
 from collections import Counter
 
 
-def prime_lists(target_gender, dataset):
-    with open(f"../Corpora/{dataset}/in_between_split.pkl", "rb") as f:
+#################################################
+########## ONE TIME PREPROCESSING STEP ##########
+#################################################
+def __expand_split_list(split_list, prime_gender):
+    expanded_split = list()
+    b_str = f"list_{prime_gender}b"
+
+    for adj_pair in split_list:
+        if sum(adj_pair["b"].values()) > 0:
+            if sum(adj_pair["a"].values()) > 0:
+                expanded_split.append((prime_gender, adj_pair["a"], adj_pair["b"], sum(adj_pair["a"].values())))
+
+            for ab in adj_pair[b_str]:
+                if sum(ab.values()) > 0:
+                    expanded_split.append((prime_gender, ab, adj_pair["b"], sum(ab.values())))
+
+    return expanded_split
+
+
+def __clean_split_list(split_list, prime_gender):
+    clean_split = list()
+    b_str = f"list_{prime_gender}b"
+
+    for adj_pair in split_list:
+        if sum(adj_pair["b"].values()) > 0 and sum(adj_pair["a"].values()) > 0:
+            clean_split.append((prime_gender, adj_pair["a"], adj_pair["b"], sum(adj_pair["a"].values())))
+
+    return clean_split
+
+
+def prep_in_between_split_list(dataset):
+    with open(f"../Corpora/{dataset}/final_split.pkl", "rb") as f:
+        mf, mm, fm, ff = pkl.load(f)
+
+    e_mf = __expand_split_list(mf, "m")
+    e_mm = __expand_split_list(mm, "m")
+    e_fm = __expand_split_list(fm, "f")
+    e_ff = __expand_split_list(ff, "f")
+    
+    with open(f"../Corpora/{dataset}/processed_in_between_split.pkl", "wb") as f:
+        pkl.dump((e_mf, e_mm, e_fm, e_ff), f)
+
+
+def prep_clean_split_list(dataset):
+    with open(f"../Corpora/{dataset}/final_split.pkl", "rb") as f:
+        mf, mm, fm, ff = pkl.load(f)
+
+    c_mf = __clean_split_list(mf, "m")
+    c_mm = __clean_split_list(mm, "m")
+    c_fm = __clean_split_list(fm, "f")
+    c_ff = __clean_split_list(ff, "f")
+    
+    with open(f"../Corpora/{dataset}/processed_clean_split.pkl", "wb") as f:
+        pkl.dump((c_mf, c_mm, c_fm, c_ff), f)
+
+
+#################################################
+#################################################
+
+
+def create_vocab(adj_pair_list):
+    vocab = set()
+
+    for _, prime, target, _ in adj_pair_list:
+        vocab |= set(prime.keys())
+        vocab |= set(target.keys())
+
+    return vocab
+
+def prime_lists(target_gender, dataset, between):
+    split_type = "in_between" if between else "clean"
+
+    with open(f"../Corpora/{dataset}/processed_{split_type}_split.pkl", "rb") as f:
         mf, mm, fm, ff = pkl.load(f)
 
     if target_gender == "m":
         return fm, mm
     else:
         return ff, mf
-
-
-# def prep_target_apl(target_gender, dataset):
-#     pf_l, pm_l = prime_lists(target_gender, dataset)
-
-#     pf_apl = list()
-#     pm_apl = list()
-
-#     for pair in pf_l:
-#         if sum(pair["a"].values()) > 0 and sum(pair["b"].values()) > 0:
-#             pf_apl.append((pair["a"], pair["b"]))
-
-#     for pair in pm_l:
-#         if sum(pair["a"].values()) > 0 and sum(pair["b"].values()) > 0:
-#             pm_apl.append((pair["a"], pair["b"]))
-        
-
-# def prep_all_apl(dataset):
-#     fm, mm = prep_target_apl("m", dataset)
-
-
-    
-
-
-# def preprocess_split(dataset):
-    
-#     return
-
-
-def create_vocab(adj_pair_list):
-    """
-    Parameters: 
-        adj_pair_list -list of tuples (gender of prime, prime counter, target counter)
-    
-    Returns:
-        set of vocab
-    """
-    vocab = set()
-
-    for _, prime, target in adj_pair_list:
-        vocab |= set(prime.keys())
-        vocab |= set(target.keys())
-
-    return vocab
